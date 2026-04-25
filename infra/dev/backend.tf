288e7aa129f8f1ff2a562ca2f9fc510956024ece
artem <tenet@Mac.lan>
2026-04-25 10:28:51 -0700

    fix0.2

--- infra/dev/backend.tf
+++ infra/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "your-terraform-state-bucket"
+    bucket         = "devops-popa-2026"
     key            = "cicd-test/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
